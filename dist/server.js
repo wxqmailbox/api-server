9f0a5b32d8b4d6513d7b47c4d406e9197dca8b05
xiaoqi.wang <wxqmailbox@hotmail.com>
2023-06-08 01:20:41 +0900

    Commit 2023/06/08

---
+++ dist/server.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("./app");
+const port = 3000 || process.env.port;
+app_1.app.listen(port, () => {
+    console.log(`API Server listening on port ${port}!`);
+});
+//# sourceMappingURL=server.js.map
---
+++ src/app.ts
@@ -0,0 +1,45 @@
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, VS Code!!!");
+});
+
+export {app};
+
+interface Task {
+    category: string;
+    title: string;
+    done: boolean;
+}
+
+const tasks: Task[] = [
+    {
+        category: "Private",
+        title:"買い物",
+        done: false,
+    }
+]
+
+app.get('/tasks', (req:Request, res:Response) => {
+    res.json(tasks);
+});
+
+app.post('/tasks', (req:Request, res:Response) => {
+    const received = req.body;
+    if ("category" in received && "title" in received && "done" in received) {
+        const newTask: Task = {
+            category: received.category,
+            title: received.title,
+            done: received.done
+        };
+        tasks.push(newTask);
+        console.log('Add:', newTask);
+        res.send("An item has been added.")
+    } else {
+        res.status(400).send("Parameters are invalid.");
+    }
+});
